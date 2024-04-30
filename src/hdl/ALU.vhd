b003bf04f85d9fc3a54bc48bd83712006f2374b0
JC <83919781+JC-max-sys@users.noreply.github.com>
2024-04-30 15:08:20 -0600

    starting place with wires

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -23,10 +23,12 @@
 --|
 --| ALU OPCODES:
 --|
---|     ADD     000
---|
---|
---|
+--|     ADD     000     A+B
+--|     SUB     001     A-B
+--|     RSHIFT  010     A>>B
+--|     LSHIFT  011     A<<B
+--|     AND     100     A and B
+--|     OR      101     A or B
 --|
 --+----------------------------------------------------------------------------
 library ieee;
@@ -35,12 +37,48 @@ library ieee;
 
 
 entity ALU is
--- TODO
+    port(
+        i_A : in std_logic_vector(7 downto 0); -- connection of A register to alu
+        i_B : in std_logic_vector(7 downto 0); -- connection of b register to alu
+        i_opcode : in std_logic_vector(2 downto 0); -- connection of opcode input to alu
+        o_result : out std_logic_vector(7 downto 0); -- output of the desired calculation based on the opcode
+        o_led : out std_logic_vector(2 downto 0) -- output of the flags
+    );
 end ALU;
 
 architecture behavioral of ALU is 
   
 	-- declare components and signals
+	
+	-- component 4 bit mux
+	
+	-- component 3 bit opcode with shift selector
+	
+	-- and
+	
+	-- or
+	
+	-- right/left shifter
+	
+	-- adder/subtractor
+	
+	-- flag 2 led converter
+	
+	-- Signals
+	signal w_A : std_logic_vector (7 downto 0); -- input wire for a
+	signal w_B : std_logic_vector (7 downto 0); -- input wire for b
+	signal w_result_final : std_logic_vector(7 downto 0); -- output for entire alu
+	signal w_result_or : std_logic_vector(7 downto 0); -- the result of or calculation
+	signal w_result_and : std_logic_vector(7 downto 0); -- the result for and calculation
+	signal w_result_shifter : std_logic_vector(7 downto 0); -- result of shift calculation
+	signal w_result_add_subtract : std_logic_vector(7 downto 0); -- result of add or subract calculation
+	signal w_shift : std_logic; -- boolean value for shift of left right or add subtract
+	signal w_opcode_select : std_logic_vector(1 downto 0); -- opcode that selects desired calculation
+	signal w_overflow : std_logic; -- boolean value for overflow
+	signal w_carry : std_logic; -- boolean value for carry
+	signal w_flag_to_led : std_logic_vector(2 downto 0); -- led output code for flags
+	
+	
 
   
 begin
--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -26,22 +26,81 @@ library ieee;
 
 
 entity top_basys3 is
--- TODO
+    port(
+        -- inputs
+        clk : in std_logic;
+        btnU : in std_logic;
+        btnC : in std_logic;
+        sw3 : in std_logic_vector(3 downto 0);
+        sw7 : in std_logic_vector(7 downto 0);
+        led_cycle : out std_logic_vector(3 downto 0);
+        seg : out std_logic_vector(7 downto 0);
+        an : out std_logic_vector(3 downto 0);
+        led_flags : out std_logic_vector(15 downto 13)
+        
+        
+        -- outputs
+
+    );
 end top_basys3;
+
+
 
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
+	  
+	
+	
+	
+	--component Register A
+	
+	
+	--component Register B
+	
+	
+	--component ALU
+	
+	
+	
+	--component MUX
+	
+	
+	--component Twos Complement to decimal converter
+	
+	
+	
+	--component TDM4
+	
+	
+	--,component Seven Segment Decoder
+	
+	
+	
+	--component Clock Divider
+	
+	
+	--component Controller FSM
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
     -- Signals --
     signal w_clk : std_logic; -- the clock signal for the mux to select a digit from the calculations
-    signal w_cycle : std_logic_vector (3 downto 0); -- the dataline for the current state, connects to output debugging leds
+    signal w_cycle_out : std_logic_vector (3 downto 0); -- the dataline for the current state, connects to output debugging leds
     signal w_reg_A : std_logic_vector (3 downto 0); -- the dataline for the first register
     signal w_reg_B : std_logic_vector (3 downto 0); -- the dataline for the second register
     signal w_flags : std_logic_vector (3 downto 0); -- the dataline for the output leds for the flags from the math
     signal w_result : std_logic_vector (3 downto 0); -- the dataline from the ALU to the mux
-    signal w_mux_to_alu : std_logic_vector (3 downto 0); -- the dataline from the mux to the binary to decimal converter box
+    signal w_mux_to_converter : std_logic_vector (3 downto 0); -- the dataline from the mux to the binary to decimal converter box
     signal w_tdm_to_7seg : std_logic_vector (3 downto 0); -- the dataline from the TDM to the Seven Segment Decoder
+    signal w_7seg_out : std_logic_vector (3 downto 0); -- dataline from seven segment decoder to the seven segment display
     signal w_sel : std_logic_vector (3 downto 0); -- the data line from the TDM which selects the anode to be on for the final output on the seven segmetn display
     -- input signals --
     signal w_sw_7_0_in : std_logic_vector ( 7 downto 0); -- the dataline from the switches 7:0 for the inputs
@@ -52,7 +111,85 @@ architecture top_basys3_arch of top_basys3 is
   
 begin
 	-- PORT MAPS ----------------------------------------
-
+        -- Register A 
+            -- inputs
+                -- w_sw_7_0_in 
+                -- w_cycle_out
+            -- outputs
+                -- w_A_out
+           
+        
+        
+    
+        -- Register B
+            -- inputs
+                 -- w_sw_7_0_in 
+                 -- w_cycle_out
+            -- outputs
+                 -- w_B_out
+        
+        -- ALU
+            -- inputs
+                -- w_reg_A
+                -- w_reg_B
+                -- w_sw_2_0_in -- this is the opcode slectlines
+            -- outputs
+                -- w_result -- result from alu
+                -- w_flags_out 
+             
+        
+        -- MUX
+            -- inputs
+               -- w_reg_A
+               -- w_reg_B
+               -- w_result
+               -- w_cycle_out
+           -- outputs 
+              -- w_mux_to_converter   
+           
+                   
+        -- Twos Complement to decimal converter
+            -- inputs
+                -- w_mux_to_converter
+            -- outputs
+                -- w_sign
+                -- w_hund
+                -- w_tens
+                -- w_ones
+                
+        
+        -- TDM4
+            -- inputs
+                -- w_sign
+                -- w_hund
+                -- w_tens
+                -- w_ones
+                -- w_clk
+           -- outputs
+                -- w_sel
+                -- w_tdm_to_7seg
+             
+        
+        -- Seven Segment Decoder
+            -- inputs
+                -- w_tdm_to_7seg
+            -- outputs
+                -- w_7seg_out
+        
+        
+        -- Clock Divider
+            -- inputs 
+                -- w_clk_in
+            -- outputs
+                -- w_clk
+        
+        
+        -- Controller FSM
+            -- inputs
+                -- w_btnU_in
+                -- w_btnC_in
+            -- outputs
+                -- w_cycle_out
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
